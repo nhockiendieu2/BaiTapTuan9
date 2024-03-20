495e568f98b085340b55c1bf6788c4b4fadc83c0
nhockiendieu2 <93941676+nhockiendieu2@users.noreply.github.com>
2024-03-20 23:05:41 +0700

    Add files via upload

--- RMIClient.java
+++ RMIClient.java
@@ -1,14 +1,34 @@
-package Tuan9;
+package Tuan92;
+
 import java.rmi.Naming;
+import java.util.List;
 
 public class RMIClient {
     public static void main(String[] args) {
         try {
-            //tim doi tuong tu may chu
-            RemoteInterface object=(RemoteInterface)
-                    Naming.lookup("//localhost:1099/RemoteObject");
-            //trieu goi tu xa
-            System.out.println(object.sayHello());
+            //tim doi tuong
+            StudentService obj=(StudentService)
+                    Naming.lookup("//localhost:1097/StudentService");
+            //goi cac phuong thuc tu xa
+            obj.addStudent("Nguyen Van A", 20);
+            obj.addStudent("Tran Van B", 21);
+            List<Student> students=obj.getAllStudents();
+            //in ra ket qua
+            System.out.println("List sinh vien");
+            for(Student std:students)
+            {
+                System.out.println("Name: "+std.getName()+
+                        " - Age: "+std.getAge());
+            }
+            //xoa ket qua
+            System.out.println("Xoa ket qua");
+            obj.removeStudent("Tran Van B");
+            System.out.println("Sau khi xoa");
+            for(Student std:students)
+            {
+                System.out.println("Name: "+std.getName()+
+                        " - Age: "+std.getAge());
+            }
         } catch (Exception e) {
             System.err.println("Loi: "+e.toString());
             e.printStackTrace();
--- RMIServer.java
+++ RMIServer.java
@@ -1,5 +1,8 @@
-package Tuan9;
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tuan92;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
@@ -7,11 +10,11 @@ public class RMIServer {
     public static void main(String[] args) {
         try {
             //khoi tao may chu
-            LocateRegistry.createRegistry(1099);
+            LocateRegistry.createRegistry(1097);
             //tao doi tuong cua lop Remote
-            RemoteInterface obj=new RemoteInterfaceImp();
+            StudentService obj=new StudentServiceImp();
             //dang ky
-            Naming.rebind("//localhost:1099/RemoteObject", obj);
+            Naming.rebind("//localhost:1097/StudentService", obj);
             System.out.println("Server da san sang");
         } catch (Exception e) {
             System.err.println("Loi: "+e.toString());
